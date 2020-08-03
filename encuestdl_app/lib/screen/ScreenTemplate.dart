f75d5bf822fc41ceeb123a198f34d84f2fd3bd24
mbmassone <mbmassone@fi.uba.ar>
2020-08-03 16:12:52 -0300

    Eliminado de PollDataWidget

--- encuestdl_app/lib/screen/ScreenTemplate.dart
+++ encuestdl_app/lib/screen/ScreenTemplate.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 class ScreenTemplate extends StatelessWidget {
   Widget child;
